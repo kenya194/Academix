ca8df08dbd1be923b22ead5f9b637418a55305f4
Astromy LLC <astromyllc@gmail.com>
2025-07-17 08:09:06 +0000

    fix: move config to app.config.js to resolve manifest placeholders issue

---
+++ app.config.js
@@ -0,0 +1,84 @@
+export default {
+  expo: {
+    name: "Solar",
+    slug: "Solar",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    scheme: "com.astromyllc.solar",
+    jsEngine: "jsc",
+    splash: {
+      image: "./assets/logo.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: ["**/*"],
+    platforms: ["ios", "android", "web"],
+    ios: {
+      supportsTablet: true,
+      jsEngine: "hermes",
+      bundleIdentifier: "com.astromyllc.solar",
+      infoPlist: {
+        CFBundleURLTypes: [
+          {
+            CFBundleURLSchemes: ["com.astromyllc.solar"]
+          }
+        ]
+      }
+    },
+    android: {
+      package: "com.astromyllc.solar",
+      permissions: [
+        "android.permission.INTERNET",
+        "android.permission.CAMERA",
+        "android.permission.READ_MEDIA_IMAGES",
+        "android.permission.READ_MEDIA_VIDEO",
+        "android.permission.READ_MEDIA_AUDIO",
+        "android.permission.READ_EXTERNAL_STORAGE"
+      ],
+      usesFeatures: [
+        {
+          name: "android.hardware.camera",
+          required: false
+        }
+      ],
+      manifestPlaceholders: {
+        appAuthRedirectScheme: "com.astromyllc.solar"
+      },
+      intentFilters: [
+        {
+          action: "VIEW",
+          data: [
+            {
+              scheme: "com.astromyllc.solar",
+              host: "oauthredirect"
+            }
+          ],
+          category: ["BROWSABLE", "DEFAULT"]
+        }
+      ]
+    },
+    plugins: [
+      "expo-secure-store",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static"
+          }
+        }
+      ],
+      "expo-asset",
+      "expo-web-browser"
+    ],
+    runtimeVersion: {
+      policy: "sdkVersion"
+    },
+    extra: {
+      eas: {
+        projectId: "35a165b7-7f76-457d-9e82-1c1a48e259fb"
+      }
+    }
+  }
+};
